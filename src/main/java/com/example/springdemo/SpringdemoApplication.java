9dec4116f9b72584e95f46b21014c28d669c0299
emersonStiven <emersonstiven1@gmail.com>
2023-05-22 18:59:27 -0500

    initializing spring boot project

---
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//it will tell spring that this is the main class
+public class SpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoApplication.class, args);
+	}
+
+}
